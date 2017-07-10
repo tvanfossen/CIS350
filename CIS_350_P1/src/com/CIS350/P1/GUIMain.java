b37e7366f5258860aca57ba214ad7ead83f7e56a
bakesauce <markbakergvsu@gmail.com>
2017-07-10 17:17:15 -0400

    JList population working
    JList can now be populated

--- CIS_350_P1/src/com/CIS350/P1/GUIMain.java
+++ CIS_350_P1/src/com/CIS350/P1/GUIMain.java
@@ -9,22 +9,29 @@ import javax.swing.JFrame;
 import javax.swing.JTabbedPane;
 import java.awt.BorderLayout;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JList;
 import javax.swing.JOptionPane;
 import javax.swing.JTextField;
+import javax.swing.ListSelectionModel;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 import javax.swing.JLabel;
+import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import javax.swing.JTextArea;
 import java.awt.GridBagLayout;
 import java.awt.GridBagConstraints;
 import java.awt.Insets;
 
-public class GUIMain {
+public class GUIMain implements ListSelectionListener {
 
 	JFrame mainFrame;
 	private JTextField keyword;
 	private JButton btnSearch;
-	private JList<String> list;
+	private JList list;
+	private DefaultListModel<String> listModel;
+	private SessionConnect newSession = new SessionConnect(GUILogin.usernameText.getText(), GUILogin.passwordText.getText());
 
 	/**
 	 * Launch the application.
@@ -54,13 +61,13 @@ public class GUIMain {
 	 */
 	private void initialize() {
 		mainFrame = new JFrame();
-		mainFrame.setBounds(100, 100, 630, 587);
+		mainFrame.setBounds(100, 100, 913, 700);
 		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		GridBagLayout gridBagLayout = new GridBagLayout();
 		gridBagLayout.columnWidths = new int[]{26, 141, 86, 84, 94, 0};
 		gridBagLayout.rowHeights = new int[]{109, 42, 24, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 		gridBagLayout.columnWeights = new double[]{0.0, 1.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
-		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
 		mainFrame.getContentPane().setLayout(gridBagLayout);
 		
 		JLabel lblSeachByKeyword = new JLabel("Seach by keyword:");
@@ -86,11 +93,19 @@ public class GUIMain {
 		gbc_btnSearch.gridx = 3;
 		gbc_btnSearch.gridy = 1;
 		mainFrame.getContentPane().add(btnSearch, gbc_btnSearch);
-	
 		
+		list = new JList();
+		listModel = new DefaultListModel<String>();
+		list = new JList<String>(listModel);
+		list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+	    list.setSelectedIndex(0);
+	    list.addListSelectionListener(this);
+	    list.setVisibleRowCount(5);
+	    //JScrollPane listScrollPane = new JScrollPane(list);
+	    
 		GridBagConstraints gbc_list = new GridBagConstraints();
-		gbc_list.gridheight = 10;
-		gbc_list.insets = new Insets(0, 0, 0, 5);
+		gbc_list.gridheight = 7;
+		gbc_list.insets = new Insets(0, 0, 5, 5);
 		gbc_list.fill = GridBagConstraints.BOTH;
 		gbc_list.gridx = 1;
 		gbc_list.gridy = 2;
@@ -98,17 +113,35 @@ public class GUIMain {
 		
 		btnSearch.addActionListener(new ActionListener() { 
 			  public void actionPerformed(ActionEvent e) {
-				  SessionConnect newSession = new SessionConnect(GUILogin.usernameText.getText(), GUILogin.passwordText.getText());
-				  ArrayList<String> temp = newSession.searchInput(keyword.getText());
-				  
-				  for (String i : temp)
-					{
-						
-					}
+
+				 listModel.removeAllElements();
+				 
+				 ArrayList<String> temp = newSession.searchInput(keyword.getText());
+				 
+				 for (String i : temp) {
+					 int index = list.getSelectedIndex();
+					 if( index == -1 ) {
+						 index = 0;
+					 } else {
+						 index++;
+					 }
+					 
+					 listModel.insertElementAt(i, index);
+				 }
+				 
+				 keyword.requestFocusInWindow();
+				 keyword.setText("");
 					
 			  } 
 			} );
+			
 		
+		
+	}
+
+	@Override
+	public void valueChanged(ListSelectionEvent arg0) {
+		// TODO Auto-generated method stub
 		
 	}
 }
